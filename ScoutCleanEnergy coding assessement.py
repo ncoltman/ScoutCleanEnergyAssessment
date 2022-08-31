0cfe384566dce51d711b3efa8cc758ecdd15bae9
ncoltman <71402434+ncoltman@users.noreply.github.com>
2022-09-01 00:11:11 +0800

    localize all the timestamps to UTC

--- ScoutCleanEnergy coding assessement.py
+++ ScoutCleanEnergy coding assessement.py
@@ -11,25 +11,41 @@ class TableData:
         self.filename = filename
 
     def excel_reader(self):
+        #assuming all files run through this program follow the same format and have id column
         table = pd.read_excel(self.filename, index_col = "id")
-        print(table)
+        return table
 
-    #def missingTimeStamp(self, table):
+    def missingTimeStamp(self, data):
+        #if we cant assume that the data is in chronological order, use this function to sort
+        #data = data.sort_values('time', ascending = True)
+        
+        time = data['time']
+        #localized to UTC (can localize to whatever timezone is wanted)
+        time_localized = pd.to_datetime(time, utc = True )
+        data_localized = data
+        data['time'] = time_localized
+        return data_localized
+        
 
-    #def erroneousValues(self, table):
+    #def erroneousValues(self):
 
     #def excel_writer(self, data, path):
 
 
 def main():
-    data = TableData("test 2021-05-06 start.xlsx")
-    data.excel_reader()
+    file = TableData("test 2021-05-06 start.xlsx")
+    data = file.excel_reader()
+    #data['time'] = pd.to_datetime(data['time'])
+    #data = data.set_index('time').asfreq('1H')
+    #print (data)
+    data_localized = file.missingTimeStamp(data)
+    print(data_localized)
+
+
+
 
 
 if __name__ == "__main__":
     main()
     print("\n")
 
-#filename = "test 2021-05-06 start.xlsx"
-#table = pd.read_excel(filename, index_col = "id")
-#print(table)
